b8c158659af9db3bb8aa15ffc4cca07f742a2845
Melanie-Thompson <Melanie.thompson@clinicalnet.com>
2025-10-20 19:19:40 +0100

    adds red lever

--- lib/screens/cube_game_screen.dart
+++ lib/screens/cube_game_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_cube/flutter_cube.dart';
+import 'dart:math' as math;
 
 class CubeGameScreen extends StatefulWidget {
   const CubeGameScreen({super.key});
@@ -9,30 +10,51 @@ class CubeGameScreen extends StatefulWidget {
 }
 
 class _CubeGameScreenState extends State<CubeGameScreen> {
-  Object? machine;
-  late Scene scene;
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
       body: Cube(
-        onSceneCreated: (Scene createdScene) {
-          scene = createdScene;
-
-          machine = Object(
+        onSceneCreated: (Scene scene) {
+          // Add machine
+          final machine = Object(
             fileName: 'assets/models/machine_converted.obj',
           );
+          machine.lighting = true;
+          scene.world.add(machine);
 
-          if (machine != null) {
-            machine!.lighting = true;
-            scene.world.add(machine!);
-          }
-
-          // Very bright light from multiple directions
-          scene.light.position.setValues(0, 20, 20);
+          // Add dial on top of machine
+          final dial = Object(
+            fileName: 'assets/models/floor_dial_converted.obj',
+          );
+          dial.lighting = false;
           
-          // Camera position
+          // Position centered on the top face of the machine
+          dial.position.setValues(0, 3.1, 0.52);
+          
+          // Fixed rotation - no skew
+          dial.rotation.setValues(-math.pi / 2, 0, 0);
+          
+          // Scale to fit nicely on the machine
+          dial.scale.setValues(0.35, 0.35, 0.35);
+          
+          dial.updateTransform();
+          scene.world.add(dial);
+
+          // Add simple lever in the center
+          final lever = Object(
+            fileName: 'assets/models/simple_lever.obj',
+          );
+          lever.lighting = true;
+          lever.position.setValues(0, 0, 0); // Center position
+          lever.scale.setValues(0.5, 0.5, 0.5);
+          lever.updateTransform();
+          scene.world.add(lever);
+
+          // Brighter lighting
+          scene.light.position.setValues(0, 50, 50);
+          
+          // Back to original camera view
           scene.camera.zoom = 12;
           scene.camera.position.setValues(0, 12, 3);
           scene.camera.target.setValues(0, 0, 0);
